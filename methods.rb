8c7e16052c29125c3024706252eca6e982349d92
Saung Oo Myat <saung.myat@gmail.com>
2022-05-30 15:13:17 -0400

    commit tree 194615402aef54dd963ed6a3e4b903225220d67d

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+greet "Jimmy"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default "Naureen" 
+greet_with_default
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+add(2, 5)
+
+def halve(num)
+    return nil unless num.class == Integer
+
+    num / 2
+end
+
+halve(6)
